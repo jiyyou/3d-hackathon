b574c358b719c56c169948df4613f75f169287b9
Jiyo <ji.yyou@gmail.com>
2020-05-29 16:25:03 -0400

    commit

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './SideBar.scss';
+import homeicon from '../../assets/icons/24-basic-home.svg';
+import basicicon from '../../assets/icons/24-basic-settings.svg';
+import clipboard from '../../assets/icons/24-file-clipboard-checked.svg';
+import filefolder from '../../assets/icons/24-file-folder-opened.svg';
+import shoppingbag from '../../assets/icons/24-shopping-bag.svg';
+import cross from '../../assets/icons/24-various-cross.svg';
 
 
 function SideBar() {
@@ -10,37 +16,37 @@ function SideBar() {
 			<ul className="navbar">
 	      <Link to={`/3d-hackathon/`}>
 					<li className="navbar__item">
-						<img className="navbar__icon" src="" alt=""/>
+						<img className="navbar__icon" src={homeicon} alt=""/>
 						<p className="navbar__link">Home</p>
 					</li>
 				</Link>
 	      <Link to={`/3d-hackathon/inventory`}>
 					<li className="navbar__item">
-							<img className="navbar__icon" src="" alt=""/>
+							<img className="navbar__icon" src={clipboard} alt=""/>
 							<p className="navbar__link">Inventory</p>
 					</li>
 				</Link>
 		    <Link to={`/3d-hackathon/`}>				
 					<li className="navbar__item">
-						<img className="navbar__icon" src="" alt=""/>
+						<img className="navbar__icon" src={shoppingbag} alt=""/>
 						<p className="navbar__link">Warehouse</p>
 					</li>
 				</Link>
 		    <Link to={`/3d-hackathon/`}>
 					<li className="navbar__item">
-						<img className="navbar__icon" src="" alt=""/>
+						<img className="navbar__icon" src={filefolder} alt=""/>
 						<p className="navbar__link">Sales Report</p>					
 					</li>
 				</Link>
 				<Link to={`/3d-hackathon/`}>
 					<li className="navbar__item">		      
-							<img className="navbar__icon" src="" alt=""/>
+							<img className="navbar__icon" src={cross} alt=""/>
 							<p className="navbar__link">Orders</p>					
 					</li>
 				</Link>
 				<Link to={`/3d-hackathon/`}>
 					<li className="navbar__item">
-		      	<img className="navbar__icon" src="" alt=""/>
+		      	<img className="navbar__icon" src={basicicon} alt=""/>
 						<p className="navbar__link">Settings</p>					
 					</li>
 				</Link>
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './Main.scss';
 import SideBar from '../../components/SideBar/SideBar';
 import UserAvatar from '../../components/UserAvatar/UserAvatar';
+import image1 from '../../assets/images/frame-16.svg';
+import image2 from '../../assets/images/frame-17.svg';
+import image3 from '../../assets/images/need-to-ship.svg';
+import image4 from '../../assets/images/global-nps.svg';
+import image5 from '../../assets/images/frame-14.svg';
+import image6 from '../../assets/images/frame-15.svg';
+
+
 
 class Main extends React.Component {
 
@@ -16,16 +24,16 @@ class Main extends React.Component {
 						<h2 className="maincontent__subtitle">Overview</h2>
 						<ul className="maincontent__scardlist">
 							<li className="maincontent__scard">
-								<h3 className="maincontent__scardtitle">Today's Order</h3>
+								<img className="maincontent__scardcontent" src={image1} alt=""/>
 							</li>
 							<li className="maincontent__scard">
-								<h3 className="maincontent__scardtitle">Today's Sales</h3>
+								<img className="maincontent__scardcontent" src={image2} alt=""/>
 							</li>
 							<li className="maincontent__scard">
-								<h3 className="maincontent__scardtitle">Need to Ship</h3>
+								<img className="maincontent__scardcontent" src={image3} alt=""/>
 							</li>
 							<li className="maincontent__scard">
-								<h3 className="maincontent__scardtitle">Require Delivery</h3>
+								<img className="maincontent__scardcontent" src={image4} alt=""/>
 							</li>
 						</ul>
 					</div>
@@ -33,13 +41,13 @@ class Main extends React.Component {
 						<div className="maincontent__bcardcont">
 							<h2 className="maincontent__subtitle">Year to Date Sales vs Last Year</h2>
 							<div className="maincontent__bcard">
-								
+								<img src={image5} alt=""/>
 							</div>
 						</div>
 						<div className="maincontent__bcardcont">
 							<h2 className="maincontent__subtitle">Current Inventory</h2>
 							<div className="maincontent__bcard">
-								
+								<img src={image6} alt=""/>
 							</div>							
 						</div>
 					</div>
