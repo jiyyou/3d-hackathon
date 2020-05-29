39fee86033aab05b5c56d9f0c352831122c1d686
Jiyo <ji.yyou@gmail.com>
2020-05-28 22:12:17 -0400

    Updates

---
+++ precache-manifest.00d0124b64be2e4a1f7730821a8bfdbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9022da8d4d7193dacaab176e93e7426",
+    "url": "/3d-hackathon/index.html"
+  },
+  {
+    "revision": "8db3c7c158e877e4428f",
+    "url": "/3d-hackathon/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b8775363531eb882a6f",
+    "url": "/3d-hackathon/static/js/2.828e0e51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/3d-hackathon/static/js/2.828e0e51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db3c7c158e877e4428f",
+    "url": "/3d-hackathon/static/js/main.14152947.chunk.js"
+  },
+  {
+    "revision": "1a00e389526b092e8b05",
+    "url": "/3d-hackathon/static/js/runtime-main.d36d8966.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/3d-hackathon/static/media/logo.5d5d9eef.svg"
+  }
+]);
